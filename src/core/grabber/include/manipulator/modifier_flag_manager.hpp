2d0f6906d6feca805ddcd582a07f58d311dcb5d6
Tony Wang <wwwjfy@gmail.com>
2016-10-09 09:56:41 +0800

    hyper key

--- src/core/grabber/include/manipulator/modifier_flag_manager.hpp
+++ src/core/grabber/include/manipulator/modifier_flag_manager.hpp
@@ -21,6 +21,7 @@ public:
     states_[static_cast<size_t>(krbn::modifier_flag::right_option)] = std::make_unique<state>("option", "⌥");
     states_[static_cast<size_t>(krbn::modifier_flag::right_command)] = std::make_unique<state>("command", "⌘");
     states_[static_cast<size_t>(krbn::modifier_flag::fn)] = std::make_unique<state>("fn", "fn");
+    states_[static_cast<size_t>(krbn::modifier_flag::hyper)] = std::make_unique<state>("hyper", "hyper");
   }
 
   void reset(void) {
@@ -119,6 +120,9 @@ public:
     if (pressed(krbn::modifier_flag::right_command)) {
       bits |= (0x1 << 7);
     }
+    if (pressed(krbn::modifier_flag::hyper)) {
+      bits |= (0x1 << 0) | (0x1 << 1) | (0x1 << 2) | (0x1 << 3);
+    }
 
     return bits;
   }
@@ -156,6 +160,9 @@ public:
     }
     if (pressed(krbn::modifier_flag::fn)) {
       bits |= NX_SECONDARYFNMASK;
+    }
+    if (pressed(krbn::modifier_flag::hyper)) {
+      bits |= NX_CONTROLMASK | NX_SHIFTMASK | NX_ALTERNATEMASK | NX_COMMANDMASK;
     }
 
     // Add NX_NUMERICPADMASK, NX_SECONDARYFNMASK by key_code.
